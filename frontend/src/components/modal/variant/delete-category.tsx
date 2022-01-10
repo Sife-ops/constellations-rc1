4090a4d98dc241a3471298523700d7a7cd9f1252
Wyatt Goettsch <wyatt.goettsch@gmail.com>
2022-01-09 16:59:35 -0800

    delete category modal

--- frontend/src/components/modal/variant/delete-category.tsx
+++ frontend/src/components/modal/variant/delete-category.tsx
@@ -6,17 +6,40 @@ import { ModalWindow } from "../modal";
 export const DeleteCategoryModal: React.FC = () => {
   const { hideModal } = React.useContext(globalContext);
 
-  const handleSubmit = () => hideModal();
+  // todo: delete category mutation
+
+  const handleConfirm = () => {
+    handleClose();
+  };
+
+  const handleClose = () => hideModal();
 
   return (
     <ModalWindow
       heading="Add Category"
-      body={<p>Are you sure you want to delete this category?</p>}
-      footer={
-        <Button variant="danger" onClick={handleSubmit}>
-          Delete
-        </Button>
+      body={
+        <>
+          <p
+            style={{
+              textAlign: "center",
+            }}
+          >
+            Are you sure you want to delete this category? This cannot be
+            undone.
+          </p>
+          <div className="d-grid gap-2 mb-2">
+            <Button variant="danger" onClick={handleConfirm}>
+              Yes, I'm sure.
+            </Button>
+          </div>
+          <div className="d-grid gap-2">
+            <Button variant="primary" onClick={handleClose}>
+              No, take me back!
+            </Button>
+          </div>
+        </>
       }
+      footer={<> </>}
     />
   );
 };

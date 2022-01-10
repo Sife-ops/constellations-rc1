ea07f93af34b5dcbc679e7c9875ab63a5cd46be5
Wyatt Goettsch <wyatt.goettsch@gmail.com>
2022-01-09 19:21:13 -0800

    delete bookmark modal

--- frontend/src/components/modal/variant/delete-bookmark.tsx
+++ frontend/src/components/modal/variant/delete-bookmark.tsx
@@ -11,17 +11,38 @@ interface Props {
 export const DeleteBookmarkModal: React.FC<Props> = ({ bookmark }) => {
   const { hideModal } = React.useContext(globalContext);
 
-  const handleSubmit = () => hideModal();
+  const handleConfirm = () => {
+    hideModal();
+  };
+
+  const handleClose = () => hideModal();
 
   return (
     <ModalWindow
       heading="Delete Bookmark"
-      body={<div>{bookmark.description}</div>}
-      footer={
-        <Button variant="danger" onClick={handleSubmit}>
-          Confirm
-        </Button>
+      body={
+        <>
+          <p
+            style={{
+              textAlign: "center",
+            }}
+          >
+            Are you sure you want to delete this bookmark? This cannot be
+            undone.
+          </p>
+          <div className="d-grid gap-2 mb-2">
+            <Button variant="danger" onClick={handleConfirm}>
+              Yes, I'm sure.
+            </Button>
+          </div>
+          <div className="d-grid gap-2">
+            <Button variant="primary" onClick={handleClose}>
+              No, take me back!
+            </Button>
+          </div>
+        </>
       }
+      footer={<></>}
     />
   );
 };

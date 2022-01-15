8b60ea10866599ec3de9db782f13a4efc3a35d1b
Wyatt Goettsch <wyatt.goettsch@gmail.com>
2022-01-15 09:47:59 -0800

    navbar

--- frontend-cra/src/App.tsx
+++ frontend-cra/src/App.tsx
@@ -2,34 +2,18 @@ import "./App.css";
 import React from "react";
 import { Dev } from "./component/dev";
 import { Home } from "./component/home";
-import { Login } from "./component/login";
-import { Register } from "./component/register";
-import { Routes, Route, Link, BrowserRouter } from "react-router-dom";
+import { NavBar } from "./component/navbar";
+import { Routes, Route, BrowserRouter } from "react-router-dom";
 
 const App: React.FC = () => {
   return (
     <div>
       <BrowserRouter>
-        <ul>
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/dev">Dev</Link>
-          </li>
-          <li>
-            <Link to="/register">Register</Link>
-          </li>
-          <li>
-            <Link to="/login">Login</Link>
-          </li>
-        </ul>
+        <NavBar />
         <div>
           <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/dev" element={<Dev />} />
-            <Route path="/register" element={<Register />} />
-            <Route path="/login" element={<Login />} />
           </Routes>
         </div>
       </BrowserRouter>
--- frontend-cra/src/component/navbar.tsx
+++ frontend-cra/src/component/navbar.tsx
@@ -1,22 +1,23 @@
 import React from "react";
+import logo from "../logo.png";
 import { Container, Nav, Navbar } from "react-bootstrap";
+import { Dev } from "./dev";
 
-export const NavBar: React.FC<{ logo: any }> = ({ logo }) => {
+export const NavBar: React.FC = () => {
   return (
     <Navbar className="mb-2" bg="primary" variant="dark">
       <Container>
         <Navbar.Brand id="id1" href="/">
           <img src={logo} width="30" height="30" /> Constellations
         </Navbar.Brand>
+
         <Nav className="me-auto">
-          <Nav.Link href="/">Home</Nav.Link>
-          <Nav.Link href="/about">About</Nav.Link>
-          <Nav.Link href="/register">Register</Nav.Link>
-          <Nav.Link href="/login">Login</Nav.Link>
           <Nav.Link href="/dev">Dev</Nav.Link>
         </Nav>
+
         <Navbar.Collapse className="justify-content-end">
           <Navbar.Text>
+            {/* todo: logout button */}
             Signed in as: <a href="/login">USER</a>
           </Navbar.Text>
         </Navbar.Collapse>

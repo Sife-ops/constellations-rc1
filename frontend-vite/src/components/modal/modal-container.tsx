4a3a793df0d6e70ce657697fad6c177109d812f4
Wyatt Goettsch <wyatt.goettsch@gmail.com>
2022-01-18 16:24:09 -0800

    remove vite

--- frontend-vite/src/components/modal/modal-container.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import { globalContext } from "../../utility/context";
-
-export const ModalContainer: React.FC = () => {
-  const {
-    globalState: { modal },
-  } = React.useContext(globalContext);
-  return <>{modal.content}</>;
-};
--- frontend-vite/src/utility/token.ts
+++
@@ -1,9 +0,0 @@
-export let accessToken: any;
-
-// export const setAccessToken = (a: any) => {
-//   accessToken = a;
-// };
-
-// export const getAccessToken = () => {
-//   return accessToken;
-// };
